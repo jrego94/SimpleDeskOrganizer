05d13d63b53eb13b01a246bdf5ed6586428d564e
jrego94 <jrego94@gmail.com>
2019-11-21 15:48:13 +0000

    Add project files.

---
+++ OrganizerDesk/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace OrganizerDesk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GBoxFrom = new System.Windows.Forms.GroupBox();
+            this.TxtPathFrom = new System.Windows.Forms.TextBox();
+            this.BtnSelectFrom = new System.Windows.Forms.Button();
+            this.GBoxTo = new System.Windows.Forms.GroupBox();
+            this.TxtPathTo = new System.Windows.Forms.TextBox();
+            this.BtnSelectTo = new System.Windows.Forms.Button();
+            this.TxtConsole = new System.Windows.Forms.TextBox();
+            this.BtnRun = new System.Windows.Forms.Button();
+            this.GBoxOpt = new System.Windows.Forms.GroupBox();
+            this.RBtnMove = new System.Windows.Forms.RadioButton();
+            this.RBtnCopy = new System.Windows.Forms.RadioButton();
+            this.BtnClearConsole = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.GBoxFrom.SuspendLayout();
+            this.GBoxTo.SuspendLayout();
+            this.GBoxOpt.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GBoxFrom
+            // 
+            this.GBoxFrom.Controls.Add(this.TxtPathFrom);
+            this.GBoxFrom.Controls.Add(this.BtnSelectFrom);
+            this.GBoxFrom.Location = new System.Drawing.Point(12, 12);
+            this.GBoxFrom.Name = "GBoxFrom";
+            this.GBoxFrom.Size = new System.Drawing.Size(776, 56);
+            this.GBoxFrom.TabIndex = 2;
+            this.GBoxFrom.TabStop = false;
+            this.GBoxFrom.Text = "From";
+            // 
+            // TxtPathFrom
+            // 
+            this.TxtPathFrom.Enabled = false;
+            this.TxtPathFrom.Location = new System.Drawing.Point(114, 22);
+            this.TxtPathFrom.Name = "TxtPathFrom";
+            this.TxtPathFrom.Size = new System.Drawing.Size(656, 20);
+            this.TxtPathFrom.TabIndex = 1;
+            // 
+            // BtnSelectFrom
+            // 
+            this.BtnSelectFrom.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BtnSelectFrom.Location = new System.Drawing.Point(7, 20);
+            this.BtnSelectFrom.Name = "BtnSelectFrom";
+            this.BtnSelectFrom.Size = new System.Drawing.Size(101, 23);
+            this.BtnSelectFrom.TabIndex = 0;
+            this.BtnSelectFrom.Text = "Select Folder";
+            this.BtnSelectFrom.UseVisualStyleBackColor = true;
+            this.BtnSelectFrom.Click += new System.EventHandler(this.BtnSelectFrom_Click);
+            // 
+            // GBoxTo
+            // 
+            this.GBoxTo.Controls.Add(this.TxtPathTo);
+            this.GBoxTo.Controls.Add(this.BtnSelectTo);
+            this.GBoxTo.Location = new System.Drawing.Point(12, 74);
+            this.GBoxTo.Name = "GBoxTo";
+            this.GBoxTo.Size = new System.Drawing.Size(776, 56);
+            this.GBoxTo.TabIndex = 3;
+            this.GBoxTo.TabStop = false;
+            this.GBoxTo.Text = "To";
+            // 
+            // TxtPathTo
+            // 
+            this.TxtPathTo.Enabled = false;
+            this.TxtPathTo.Location = new System.Drawing.Point(114, 22);
+            this.TxtPathTo.Name = "TxtPathTo";
+            this.TxtPathTo.Size = new System.Drawing.Size(656, 20);
+            this.TxtPathTo.TabIndex = 1;
+            // 
+            // BtnSelectTo
+            // 
+            this.BtnSelectTo.Location = new System.Drawing.Point(7, 20);
+            this.BtnSelectTo.Name = "BtnSelectTo";
+            this.BtnSelectTo.Size = new System.Drawing.Size(101, 23);
+            this.BtnSelectTo.TabIndex = 0;
+            this.BtnSelectTo.Text = "Select Folder";
+            this.BtnSelectTo.UseVisualStyleBackColor = true;
+            this.BtnSelectTo.Click += new System.EventHandler(this.BtnSelectTo_Click);
+            // 
+            // TxtConsole
+            // 
+            this.TxtConsole.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtConsole.HideSelection = false;
+            this.TxtConsole.Location = new System.Drawing.Point(12, 227);
+            this.TxtConsole.Multiline = true;
+            this.TxtConsole.Name = "TxtConsole";
+            this.TxtConsole.ReadOnly = true;
+            this.TxtConsole.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TxtConsole.Size = new System.Drawing.Size(776, 280);
+            this.TxtConsole.TabIndex = 4;
+            // 
+            // BtnRun
+            // 
+            this.BtnRun.Location = new System.Drawing.Point(12, 198);
+            this.BtnRun.Name = "BtnRun";
+            this.BtnRun.Size = new System.Drawing.Size(621, 23);
+            this.BtnRun.TabIndex = 5;
+            this.BtnRun.Text = "Organize";
+            this.BtnRun.UseVisualStyleBackColor = true;
+            this.BtnRun.Click += new System.EventHandler(this.BtnRun_Click);
+            // 
+            // GBoxOpt
+            // 
+            this.GBoxOpt.Controls.Add(this.RBtnMove);
+            this.GBoxOpt.Controls.Add(this.RBtnCopy);
+            this.GBoxOpt.Location = new System.Drawing.Point(13, 137);
+            this.GBoxOpt.Name = "GBoxOpt";
+            this.GBoxOpt.Size = new System.Drawing.Size(775, 55);
+            this.GBoxOpt.TabIndex = 6;
+            this.GBoxOpt.TabStop = false;
+            this.GBoxOpt.Text = "Options";
+            // 
+            // RBtnMove
+            // 
+            this.RBtnMove.AutoSize = true;
+            this.RBtnMove.Location = new System.Drawing.Point(61, 19);
+            this.RBtnMove.Name = "RBtnMove";
+            this.RBtnMove.Size = new System.Drawing.Size(52, 17);
+            this.RBtnMove.TabIndex = 1;
+            this.RBtnMove.Text = "Move";
+            this.RBtnMove.UseVisualStyleBackColor = true;
+            // 
+            // RBtnCopy
+            // 
+            this.RBtnCopy.AutoSize = true;
+            this.RBtnCopy.Checked = true;
+            this.RBtnCopy.Location = new System.Drawing.Point(6, 19);
+            this.RBtnCopy.Name = "RBtnCopy";
+            this.RBtnCopy.Size = new System.Drawing.Size(49, 17);
+            this.RBtnCopy.TabIndex = 0;
+            this.RBtnCopy.TabStop = true;
+            this.RBtnCopy.Text = "Copy";
+            this.RBtnCopy.UseVisualStyleBackColor = true;
+            // 
+            // BtnClearConsole
+            // 
+            this.BtnClearConsole.Location = new System.Drawing.Point(639, 198);
+            this.BtnClearConsole.Name = "BtnClearConsole";
+            this.BtnClearConsole.Size = new System.Drawing.Size(149, 23);
+            this.BtnClearConsole.TabIndex = 7;
+            this.BtnClearConsole.Text = "Clear Console";
+            this.BtnClearConsole.UseVisualStyleBackColor = true;
+            this.BtnClearConsole.Click += new System.EventHandler(this.BtnClearConsole_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 513);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(776, 19);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 544);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.BtnClearConsole);
+            this.Controls.Add(this.GBoxOpt);
+            this.Controls.Add(this.BtnRun);
+            this.Controls.Add(this.TxtConsole);
+            this.Controls.Add(this.GBoxTo);
+            this.Controls.Add(this.GBoxFrom);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Form1";
+            this.GBoxFrom.ResumeLayout(false);
+            this.GBoxFrom.PerformLayout();
+            this.GBoxTo.ResumeLayout(false);
+            this.GBoxTo.PerformLayout();
+            this.GBoxOpt.ResumeLayout(false);
+            this.GBoxOpt.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox GBoxFrom;
+        private System.Windows.Forms.TextBox TxtPathFrom;
+        private System.Windows.Forms.Button BtnSelectFrom;
+        private System.Windows.Forms.GroupBox GBoxTo;
+        private System.Windows.Forms.TextBox TxtPathTo;
+        private System.Windows.Forms.Button BtnSelectTo;
+        private System.Windows.Forms.TextBox TxtConsole;
+        private System.Windows.Forms.Button BtnRun;
+        private System.Windows.Forms.GroupBox GBoxOpt;
+        private System.Windows.Forms.RadioButton RBtnMove;
+        private System.Windows.Forms.RadioButton RBtnCopy;
+        private System.Windows.Forms.Button BtnClearConsole;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ OrganizerDesk/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace OrganizerDesk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            backgroundWorker1.DoWork += backgroundWorker1_DoWork;
+            backgroundWorker1.ProgressChanged += backgroundWorker1_ProgressChanged;
+            backgroundWorker1.RunWorkerCompleted += backgroundWorker1_RunWorkerCompleted;  //Tell the user how the process went
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+        }
+
+        public string PathFrom { get; private set; }
+        public string PathTo { get; private set; }
+        private static readonly Regex r = new Regex(":");
+
+        private void BtnSelectFrom_Click(object sender, EventArgs e)
+        {
+            //open dialog
+            OpenFileDialog folderBrowser = new OpenFileDialog();
+            // Set validate names and check file exists to false otherwise windows will
+            // not let you select "Folder Selection."
+            folderBrowser.ValidateNames = false;
+            folderBrowser.CheckFileExists = false;
+            folderBrowser.CheckPathExists = true;
+            // Always default to Folder Selection.
+            folderBrowser.FileName = "Folder";
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                PathFrom = Path.GetDirectoryName(folderBrowser.FileName);
+                TxtPathFrom.Text = PathFrom;
+
+                int fCount = Directory.GetFiles(PathFrom, "*", SearchOption.AllDirectories).Length;
+                int dCount = Directory.GetDirectories(PathFrom, "*", SearchOption.AllDirectories).Length;
+
+                TxtConsole.AppendText("Path From: " + PathFrom + Environment.NewLine +
+                    fCount + " Files" + Environment.NewLine +
+                    dCount + " Directories" + Environment.NewLine
+                    );
+            }
+        }
+
+        private void BtnSelectTo_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog folderBrowser = new OpenFileDialog();
+            // Set validate names and check file exists to false otherwise windows will
+            // not let you select "Folder Selection."
+            folderBrowser.ValidateNames = false;
+            folderBrowser.CheckFileExists = false;
+            folderBrowser.CheckPathExists = true;
+            // Always default to Folder Selection.
+            folderBrowser.FileName = "Folder Selection.";
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                PathTo = Path.GetDirectoryName(folderBrowser.FileName);
+                TxtPathTo.Text = PathTo;
+
+                int fCount = Directory.GetFiles(PathTo, "*", SearchOption.AllDirectories).Length;
+                int dCount = Directory.GetDirectories(PathTo, "*", SearchOption.AllDirectories).Length;
+
+                TxtConsole.AppendText("Path To: " + PathFrom + Environment.NewLine +
+                    fCount + " Files" + Environment.NewLine +
+                    dCount + " Directories" + Environment.NewLine
+                    );
+            }
+        }
+
+        private void BtnRun_Click(object sender, EventArgs e)
+        {
+            setEnableOnOFf(false);//disable groupbox,btn and txt
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        void DirSearch(string sDir)
+        {
+            try
+            {
+                // get the directories of the chosen path.
+                // If there is any directory, enter the cycle and call it itself until there is no directory.
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    DirSearch(d);
+                }
+
+                //then goes through all the files in the directory you are in
+                foreach (string g in Directory.GetFiles(sDir, "*.*"))
+                {
+                    FileInfo i = new FileInfo(g);
+                    MovFiles(i.FullName, i.Name, GetDateTakenFromImage(g));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                backgroundWorker1.ReportProgress(0, String.Format("Catch # {0}", ex));
+            }
+
+        }
+
+        //https://stackoverflow.com/questions/180030/how-can-i-find-out-when-a-picture-was-actually-taken-in-c-sharp-running-on-vista
+        public static DateTime GetDateTakenFromImage(string path)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+                using (Image myImage = Image.FromStream(fs, false, false))
+                {
+                    PropertyItem propItem = null;
+                    try
+                    {
+                        propItem = myImage.GetPropertyItem(36867);
+                    }
+                    catch { }
+                    if (propItem != null)
+                    {
+                        string dateTaken = r.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
+                        return DateTime.Parse(dateTaken);
+                    }
+                    else
+                        return new FileInfo(path).CreationTime;
+                }
+            }
+            catch (Exception)
+            {
+                return new FileInfo(path).CreationTime;
+            }
+
+        }
+
+
+        private void MovFiles(string filePath, string name, DateTime date)
+        {
+            string pathyear = PathTo + "/" + date.Year;
+            string pathmouth = pathyear + "/" + date.Month;
+            string pathday = pathmouth + "/" + date.Day;
+            try
+            {
+                if (!Directory.Exists(pathyear))
+                {
+                    Directory.CreateDirectory(pathyear);
+                }
+
+                if (!Directory.Exists(pathmouth))
+                {
+                    Directory.CreateDirectory(pathmouth);
+                }
+
+                if (!Directory.Exists(pathday))
+                {
+                    Directory.CreateDirectory(pathday);
+                }
+
+                if (RBtnCopy.Checked)
+                {
+                    if (!File.Exists(pathday + "/" + name))
+                    {
+                        File.Copy(filePath, pathday + "/" + name);
+                        backgroundWorker1.ReportProgress(0, String.Format("Copied # {0}", filePath));
+                    }
+                    else
+                    {
+                        backgroundWorker1.ReportProgress(0, String.Format("Not copied # {0}", filePath));
+                    }
+
+                }
+                else if (RBtnMove.Checked)
+                {
+                    if (!File.Exists(pathday + "/" + name))
+                    {
+                        File.Move(filePath, pathday + "/" + name);
+                        backgroundWorker1.ReportProgress(0, String.Format("Moved # {0}", filePath));
+                    }
+                    else
+                    {
+                        backgroundWorker1.ReportProgress(0, String.Format("Not moved # {0}", filePath));
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                backgroundWorker1.ReportProgress(0, String.Format("Error # {0}", ex));
+            }
+
+        }
+
+        private void BtnClearConsole_Click(object sender, EventArgs e)
+        {
+            TxtConsole.Text = "";
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            if (PathFrom != null && PathTo != null)
+            {
+                DirSearch(PathFrom);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            TxtConsole.AppendText(e.UserState as string + Environment.NewLine);
+            TxtConsole.SelectionStart = TxtConsole.Text.Length;
+            TxtConsole.ScrollToCaret();
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                TxtConsole.AppendText("Process was cancelled" + Environment.NewLine);
+            }
+            else if (e.Error != null)
+            {
+                TxtConsole.AppendText("There was an error running the process. The thread aborted" + Environment.NewLine);
+            }
+            else
+            {
+                TxtConsole.AppendText("Process was completed" + Environment.NewLine);
+
+            }
+            setEnableOnOFf(true);
+
+            TxtConsole.SelectionStart = TxtConsole.Text.Length;
+            TxtConsole.ScrollToCaret();
+        }
+
+        private void setEnableOnOFf(Boolean b)
+        {
+            GBoxFrom.Enabled = b;
+            GBoxTo.Enabled = b;
+            GBoxFrom.Enabled = b;
+            RBtnCopy.Enabled = b;
+            RBtnMove.Enabled = b;
+            BtnClearConsole.Enabled = b;
+            BtnRun.Enabled = b;
+        }
+
+    }
+}
